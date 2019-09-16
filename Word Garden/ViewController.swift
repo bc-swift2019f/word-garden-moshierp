8ad4c9fd63a4d09796f935d526070a9b0ab9dba9
Paige Moshier <moshierp@bc.edu>
2019-09-15 23:20:06 -0400

    Version 3.3

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("In viewDidLoad, is guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
-        
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
         
     }
     func updateUIAfterGuess(){
@@ -29,6 +29,13 @@ class ViewController: UIViewController {
     }
    
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        if let letterGuessed = guessedLetterField.text?.last {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            //disable the button if i dont have a single character in the guessedLetterField
+            guessLetterButton.isEnabled = false
+        }
     }
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
